2ed06c10f89b86c5bc88598435d8f772247dadcc
jugo <jugo.tw@gmail.com>
2015-11-19 00:36:22 +0800

    add

---
+++ src/org/iii/smartcharge/handler/ViewPagerHandler.java
@@ -0,0 +1,146 @@
+package org.iii.smartcharge.handler;
+
+import org.iii.smartcharge.R;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
+
+public class ViewPagerHandler extends BaseHandler
+{
+	private ViewPager			viewPager		= null;
+	private ViewPagerAdapter	pagerAdapter	= null;
+	public static int			PAGE_POWER;
+	public static int			PAGE_TEMPERATURE;
+	public static int			PAGE_HEALTH;
+	public static int			PAGE_VOLTAGE;
+
+	public ViewPagerHandler(Activity activity, Handler handler)
+	{
+		super(activity, handler);
+	}
+
+	public boolean init()
+	{
+		viewPager = (ViewPager) theActivity.findViewById(R.id.ViewPager);
+
+		if (null == viewPager)
+		{
+			return false;
+		}
+
+		viewPager.setOnTouchListener(new OnTouchListener()
+		{
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event)
+			{
+				return true;
+			}
+		});
+
+		LayoutInflater Inflater = LayoutInflater.from(theActivity);
+		pagerAdapter = new ViewPagerAdapter();
+
+		PAGE_POWER = pagerAdapter.addPage(Inflater.inflate(R.layout.power_level, null),
+				theActivity.getString(R.string.power_level));
+		PAGE_TEMPERATURE = pagerAdapter.addPage(Inflater.inflate(R.layout.battery_temperature, null),
+				theActivity.getString(R.string.temperature));
+		PAGE_HEALTH = pagerAdapter.addPage(Inflater.inflate(R.layout.battery_health, null),
+				theActivity.getString(R.string.health));
+		PAGE_VOLTAGE = pagerAdapter.addPage(Inflater.inflate(R.layout.battery_voltage, null),
+				theActivity.getString(R.string.voltage));
+
+		viewPager.setAdapter(pagerAdapter);
+		return true;
+	}
+
+	public View getView(final int nId)
+	{
+		return pagerAdapter.getView(nId);
+	}
+
+	public void showPage(final int nPageIndex)
+	{
+		if (0 > nPageIndex || pagerAdapter.getCount() <= nPageIndex)
+			return;
+
+		viewPager.setCurrentItem(nPageIndex, true);
+	}
+
+	private class ViewPagerAdapter extends PagerAdapter
+	{
+
+		private class Page
+		{
+			public View		view		= null;
+			public String	strTitle	= null;
+		}
+
+		private SparseArray<Page> Pages = null;
+
+		public ViewPagerAdapter()
+		{
+			Pages = new SparseArray<Page>();
+		}
+
+		@Override
+		public int getCount()
+		{
+			return Pages.size();
+		}
+
+		@Override
+		public boolean isViewFromObject(View arg0, Object arg1)
+		{
+			return (arg0 == arg1);
+		}
+
+		@Override
+		public int getItemPosition(Object object)
+		{
+			return super.getItemPosition(object);
+		}
+
+		@Override
+		public CharSequence getPageTitle(int position)
+		{
+			return Pages.get(position).strTitle;
+		}
+
+		@Override
+		public void destroyItem(ViewGroup container, int position, Object object)
+		{
+			container.removeView(Pages.get(position).view);
+		}
+
+		@Override
+		public Object instantiateItem(ViewGroup container, int position)
+		{
+			((ViewPager) container).addView(Pages.get(position).view, 0);
+			return Pages.get(position).view;
+		}
+
+		public int addPage(final View view, final String strTitle)
+		{
+			Page page = new Page();
+			page.view = view;
+			page.strTitle = strTitle;
+			Pages.put(Pages.size(), page);
+			return (Pages.size() - 1);
+		}
+
+		public View getView(final int nId)
+		{
+			return Pages.get(nId).view;
+		}
+
+	}
+}
---
+++ src/org/iii/smartcharge/view/TemperatureGaugeView.java
@@ -0,0 +1,100 @@
+package org.iii.smartcharge.view;
+
+import org.iii.smartcharge.R;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.util.AttributeSet;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ImageView.ScaleType;
+
+public class TemperatureGaugeView extends RelativeLayout
+{
+
+	private int	mnStartAmount	= -135;
+	private int	mnEndAmount		= 135;
+	ImageView	ivPointer		= null;
+	ImageView	ivCenter		= null;
+
+	public TemperatureGaugeView(Context context)
+	{
+		super(context);
+		init(context);
+	}
+
+	public TemperatureGaugeView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		init(context);
+	}
+
+	public TemperatureGaugeView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+		init(context);
+	}
+
+	private void init(Context context)
+	{
+		ivPointer = new ImageView(context);
+		ivPointer.setScaleType(ScaleType.CENTER_INSIDE);
+		ivPointer.setAdjustViewBounds(false);
+		//ivPointer.setImageResource(R.drawable.pointer);
+		ivPointer.setBackgroundColor(Color.argb(155, 255, 50, 50));
+		RelativeLayout.LayoutParams ivPointerlayoutParams = new RelativeLayout.LayoutParams(12,
+				LayoutParams.MATCH_PARENT);
+		ivPointerlayoutParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+		ivPointerlayoutParams.setMargins(8, 80, 0, 200);
+		ivPointer.setLayoutParams(ivPointerlayoutParams);
+		addView(ivPointer);
+
+		ivCenter = new ImageView(context);
+		ivCenter.setScaleType(ScaleType.CENTER_INSIDE);
+		ivCenter.setAdjustViewBounds(false);
+		ivCenter.setImageResource(R.drawable.circle);
+		RelativeLayout.LayoutParams ivCenterlayoutParams = new RelativeLayout.LayoutParams(60, 60);
+		ivCenterlayoutParams.addRule(RelativeLayout.CENTER_IN_PARENT);
+		ivCenter.setLayoutParams(ivCenterlayoutParams);
+		addView(ivCenter);
+	}
+
+	public void setAmount(int nAmount)
+	{
+		ivPointer.clearAnimation();
+		mnEndAmount = nAmount;
+		RotateAnimation rotate = new RotateAnimation(mnStartAmount, mnEndAmount, Animation.RELATIVE_TO_SELF, 0.5f,
+				Animation.RELATIVE_TO_SELF, 1.0f);
+		rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+		rotate.setDuration(1000);
+		rotate.setAnimationListener(listener);
+		rotate.setFillAfter(true);
+		ivPointer.setAnimation(rotate);
+	}
+
+	private AnimationListener listener = new AnimationListener()
+	{
+
+		@Override
+		public void onAnimationStart(Animation animation)
+		{
+
+		}
+
+		@Override
+		public void onAnimationEnd(Animation animation)
+		{
+			mnStartAmount = mnEndAmount;
+		}
+
+		@Override
+		public void onAnimationRepeat(Animation animation)
+		{
+
+		}
+	};
+}
